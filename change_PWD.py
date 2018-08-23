5dbff9a3824cc350def302f07530413225d0b925
Sam Chen <free6323@hotmail.com>
2018-08-23 23:06:54 +0800

    login part

---
+++ change_PWD.py
@@ -0,0 +1,16 @@
+password = 'a123456'
+password_time = 3
+while password_time <= 3:
+	key_password = input('請輸入密碼: ')
+	if key_password == 'a123456':
+		print('登入成功')
+		break
+	else:
+		password_time = password_time - 1
+		if password_time <= 0:
+			print('結束程式')
+			break
+		print('輸入錯誤')
+		print('請再輸入密碼,還有', password_time, '次機會')
+		
+		
